325eeebea8390f853e53d48bafd0f7bb9fb8b68c
Rahul Yadav <91110171+rahulyadav139@users.noreply.github.com>
2022-01-03 17:45:41 +0530

    Update Filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -78,6 +78,7 @@ const Filter = props => {
               key={el.name}
               href={`https://twitter.com/${el.twitter}`}
               target="_blank"
+               rel="no-referrer"
             >
               <div className={styles.card}>
                 <div className={styles.image}>
